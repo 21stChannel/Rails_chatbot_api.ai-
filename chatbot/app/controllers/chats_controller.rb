5582f716acd4cc474d785aff50909038a3a4a9f3
Ryan <haruhiku@Ryans-MacBook-Pro.local>
2017-05-12 13:03:52 -0700

    ver1

---
+++ chatbot/app/controllers/chats_controller.rb
@@ -0,0 +1,20 @@
+class ChatsController < ApplicationController
+  def index
+  end
+
+  def create
+    # set your api.ai access token
+    client = ApiAiRuby::Client.new(
+    :client_access_token => 'ACCESS_TOKEN'
+    )
+    @request = params[:chatContent]
+    # Should not send blank request to api.ai
+    if @request.length < 1
+      @request = "(I don't know what to say)"
+    end
+    @JSONResponse = client.text_request @request
+    # get the text response only
+    @response = @JSONResponse[:result][:fulfillment][:speech]
+    render :action => "show"
+  end
+end
