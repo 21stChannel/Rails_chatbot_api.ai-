490efc89ebb69e6ec8581ebd77be6c1a87754bc5
haruhiku <haruhiku@gmail.com>
2017-05-13 11:41:08 -0700

    Update chats_controller.rb
    fix chat sequence

--- chatbot/app/controllers/chats_controller.rb
+++ chatbot/app/controllers/chats_controller.rb
@@ -1,18 +1,18 @@
 class ChatsController < ApplicationController
+  # set your api.ai access token
+  @@client = ApiAiRuby::Client.new(
+    :client_access_token => 'ACCESS_TOKEN'
+    )
   def index
   end
 
   def create
-    # set your api.ai access token
-    client = ApiAiRuby::Client.new(
-    :client_access_token => 'ACCESS_TOKEN'
-    )
     @request = params[:chatContent]
     # Should not send blank request to api.ai
     if @request.length < 1
       @request = "(I don't know what to say)"
     end
-    @JSONResponse = client.text_request @request
+    @JSONResponse = @@client.text_request @request
     # get the text response only
     @response = @JSONResponse[:result][:fulfillment][:speech]
     render :action => "show"
